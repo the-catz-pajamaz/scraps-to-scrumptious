57a717153ed3aaedbb16c90227bf7a26618f60e6
Dillon Ginsburg <dillon.ginsburg@gmail.com>
2019-05-01 16:03:05 -0600

    power user persona

---
+++ epic/power-persona.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta charset="UTF-8">
+		<title>scraps-to-scrumptious</title>
+	</head>
+
+		<h1>Persona</h1>
+		<ul>
+			<li><strong>Name:</strong>Chef Jon</li>
+			<li><strong>Age:</strong>33</li>
+			<li><strong>Profession:</strong>Professional Chef</li>
+			<li><strong>Technology:</strong>iPhone 6s (because it has a headphone jack), 2013 dell laptop with windows 7 pro. </li>
+			<li><strong>Attitudes and Behaviors:</strong>Calm, focused, and driven. In his profession for the food, not the money. Chef Jon gets all of his produce locally, and needs to creatively integrate unusual ingredients into his dishes.</li>
+			<li><strong>Frustrations and Needs:</strong>Chef Jon wants to be able to share his recipes with the world, and use key ingredients to find others' recipes as well.</li>
+			<li><strong>Goals:</strong>Utilize scaps-to-scrumptious to share and find recipes.</li>
+			<li><strong>User Story:</strong>As a power user, I want to be able to upload my recipes and search for others' recipes based on specific ingredients.</li>
+
+		</ul>
+	</body>
+
+</html>
