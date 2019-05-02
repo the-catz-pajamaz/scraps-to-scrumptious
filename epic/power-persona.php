7bda3db5f7a1736ccff87243cf02aea528b92ff8
Dillon Ginsburg <dillon.ginsburg@gmail.com>
2019-05-02 14:11:47 -0600

    power user persona/case

---
+++ epic/power-persona.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta charset="UTF-8">
+		<title>scraps-to-scrumptious</title>
+	</head>
+		<h1>Power User</h1>
+	<body>
+		<h2>Persona</h2>
+		<ul>
+			<li><strong>Name:</strong>Chef Jon</li>
+			<li><strong>Age:</strong>33</li>
+			<li><strong>Profession:</strong>Professional Chef</li>
+			<li><strong>Technology:</strong>iPhone 6s (because it has a headphone jack), 2013 dell laptop with windows 7 pro. </li>
+			<li><strong>Attitudes and Behaviors:</strong>Calm, focused, and driven. In his profession for the food, not the money. Chef Jon gets all of his produce locally, and needs to creatively integrate unusual ingredients into his dishes.</li>
+			<li><strong>Frustrations and Needs:</strong>Chef Jon wants to be able to share his recipes with the world, and use key ingredients to find others' recipes as well.</li>
+			<li><strong>Goals:</strong>Utilize scaps-to-scrumptious to share and find recipes.</li>
+		</ul>
+		<h2><strong>User Story:</strong>As a power user, I want to be able to upload my recipes and search for others' recipes based on specific ingredients.</h2>
+		<h2>User Interaction Flow:</h2>
+		<p>Preconditions: Jon has an account with scraps-to-scrumptious and is logged in.</p>
+		<p>Postconditions: Jon uploads a recipe</p>
+
+			<ul>
+				<li>Jon clicks on "add to my cookbook"</li>
+				<li>He is first prompted to add his ingredients (separated by commas), and then prompted to type  the rest of the recipe</li>
+				<li>The Chef clicks on "finished" and the recipe is added to his cookbook.</li>
+				<li>Jon navigates back to the home page and locates the "find a recipe" search bar.</li>
+				<li>He then types "orc meat" into the search bar and is displayed the top recipes with the requested ingredient.</li>
+				<li>He selects the recipe of his desire and saves it to his cookbook.</li>
+			</ul>
+	</body>
+
+</html>
