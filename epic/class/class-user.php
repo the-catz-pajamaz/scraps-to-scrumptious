aaf7cf97a60b8df08cd3263eec8573422edc9b4f
Dillon Ginsburg <dillon.ginsburg@gmail.com>
2019-05-08 13:13:52 -0600

    added accessors and mutators for user email, userFirstName, UserHandle, userHash, and userLastName. all accessors and mutators are complete for this class.

--- epic/class/class-user.php
+++ epic/class/class-user.php
@@ -20,8 +20,9 @@ user{
 
 	//constructor method for table user
 
-	public function __construct ($newUserId, string $newUserActivationToken, string $newUserEmail, string $newUserFirstName,
-										  string $newUserHandle, string $newUserHash, string $newUserLastName) {
+	public function __construct ($newUserId, string $newUserActivationToken, string $newUserEmail,
+										  string $newUserFirstName, string $newUserHandle, string $newUserHash,
+										  string $newUserLastName) {
 		try{
 			$this->setUserId($newUserId);
 			$this->setUserActivationToken($newUserActivationToken);
@@ -69,7 +70,7 @@ user{
 	/**
 	 * Accessor method for userActivationToken
 	 */
-	public function getUserActivationToken() {
+	public function getUserActivationToken(): ?string {
 		return $this->userActivationToken;
 	}
 
@@ -94,8 +95,128 @@ user{
 	/**
 	 * accessor method for userEmail :)
 	 */
-	public function getUserEmail() {
+	public function getUserEmail():?string {
 		return $this->userEmail;
 	}
 
+
+	/**
+	 *mutator method for userEmail
+	 *
+	 *@param string $newUserEmail new email
+	 *@throws \ InvalidArgumentException if $newEmail is not valid email or insecure
+	 *@throws \RangeException if $newEmail is >128 characters
+	 *@throws \TypeError if $newEmail is not a string
+	 */
+
+	public function setUserEmail( string $newUserEmail): void{
+		$newUserEmail = trim($newUserEmail);
+		$newUserEmail = filter_var($newUserEmail, FILTER_VALIDATE_EMAIL);
+		if(empty($newUserEmail) === true) {
+			throw(new \InvalidArgumentException("userEmail is empty or insecure"));
+		}
+		if(strlen($newUserEmail) >128) {
+			throw(new \RangeException("User Email is too large"));
+		}
+		$this->userEmail = $newUserEmail;
+	}
+
+	/**
+	 * accessor method for userFirstName
+	 *
+	 */
+	public function getUserFirstName():?string {
+		return $this->userFirstName;
+	}
+
+	/**
+	 * Mutator Method for userFirstName
+	 * @param string
+	 * @throw range exception
+	 */
+	public function setUserFirstName($newUserFirstName): void {
+		$newUserFirstName = trim($newUserFirstName);
+		if(strlen($newUserFirstName)>32){
+			throw(new \RangeException("First Name Not Valid"));
+		}
+		$this->userFirstName = $newUserFirstName;
+	}
+
+	/**
+	 * accessor method for userHandle
+	 */
+	public function getUserHandle():?string {
+		return $this->userHandle;
+	}
+
+	/**
+	 * mutator method for UserHandle
+	 * @param string
+	 * @throw range exception
+	 */
+	public function setUserHandle($newUserHandle): void {
+		$newUserHandle = trim($newUserHandle);
+		if(strlen($newUserHandle)>64){
+			throw(new \RangeException("User Handle Not Valid"));
+		}
+		$this->userHandle = $newUserHandle;
+	}
+
+	/**
+	 *accessor method for user hash
+	 *
+	 * @return string value userHash
+	 */
+
+	public function getUserHash():?string {
+		return $this->userHash;
+	}
+
+	/**
+	 * Mutator method for user hash
+	 * @param string $newUserHash
+	 * @throws \InvalidArgumentException if the hash is not secure
+	 * @throws \RangeException if the hash is >97 characters
+	 */
+	public function setUserHash(string $newUserHash): void {
+		//enforce hash formatting
+		$newUserHash = trim($newUserHash);
+		if(empty($newUserHash) === true) {
+			throw(new \InvalidArgumentException("User hash is not a valid hash"));
+		}
+		//enforce that it is an argon hash
+		$newUserHashInfo = password_get_info($newUserHash);
+		if($newUserHashInfo["algoName"] !== "argon2i") {
+			throw(new \InvalidArgumentException("user hash is not a valid hash"));
+		}
+		if(strlen($newUserHash) !== 97) {
+			throw(new \RangeException("user hash must be 97 characters"));
+		}
+		$this->userHash = $newUserHash;
+	}
+
+	/**
+	 * accessor method for userLastName
+	 *
+	 */
+	public function getUserLastName():?string {
+		return $this->userLastName;
+	}
+
+	/**
+	 * Mutator Method for userFirstName
+	 * @param string
+	 * @throw range exception
+	 */
+	public function setUserLastName($newUserLastName): void {
+		$newUserLastName = trim($newUserLastName);
+		if(strlen($newUserLastName)>32){
+			throw(new \RangeException("First Name Not Valid"));
+		}
+		$this->userLastName = $newUserLastName;
+	}
+	
+	
+	
+	
 }
