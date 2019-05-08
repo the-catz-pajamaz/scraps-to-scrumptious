ca1d6eae0df75bf5a67d3ecc6b9b4156298570c9
Samuel Nelson <snelson54@cnm.edu>
2019-05-08 11:05:23 -0600

    Finished construct function by adding 'try' and 'catch'.

--- class-cookbook/class-cookbook.php
+++ class-cookbook/class-cookbook.php
@@ -20,7 +20,11 @@ class Cookbook {
 	 */
 	public function __construct(string $newCookbookRecipeId, string $newCookbookUserId) {
 		try {
-
+			$this->cookbookRecipeId($newCookbookRecipeId);
+			$this->cookbookUserId($newCookbookUserId);
+		}
+		catch(\InvalidArgumentException | \RangeException | \Exception | \TypeError $exception) {
+			echo "Invalid request, Id not found";
 		}
 	}
 }
