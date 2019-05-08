dd8b5d66a230120f411291acb5cee933f818d167
Samuel Nelson <snelson54@cnm.edu>
2019-05-08 14:45:26 -0600

    Created insert statment

--- class-cookbook/class-cookbook.php
+++ class-cookbook/class-cookbook.php
@@ -16,22 +16,78 @@ class Cookbook {
 	 * @var cookbookUserId
 	 */
 	/**
-	 * constructor for author
+	 * constructor for cookbook
 	 */
 	public function __construct(string $newCookbookRecipeId, string $newCookbookUserId) {
 		try {
 			$this->cookbookRecipeId($newCookbookRecipeId);
 			$this->cookbookUserId($newCookbookUserId);
-		}
-		catch(\InvalidArgumentException | \RangeException | \Exception | \TypeError $exception) {
+		} catch(\InvalidArgumentException | \RangeException | \Exception | \TypeError $exception) {
 			echo "Invalid request, Id not found";
 		}
 	}
+
 	/**
 	 * accessor method for cookbookRecipeId
 	 * @return Uuid value of cookbookRecipeId
 	 */
 	public function getCookbookRecipeId(): Uuid {
 		return ($this->cookbookRecipeId);
+	}
+
+	/**
+	 * mutator method for cookbookRecipeId
+	 *
+	 * @param Uuid | string $newCookbookRecipeId value of cookbookRecipeId
+	 * @throws \RangeException if $newCookbookRecipeId is not positive
+	 * @throws \TypeError if the cookbookRecipeId is not a string
+	 */
+	public function setCookbookRecipeId($cookbookRecipeId): void {
+		try {
+			$uuid = self::validateUuid($newCookbookRecipeId);
+		} catch(\InvalidArgumentException | \RangeException | \Exception | \TypeError $exception) {
+			$exceptionType = get_class($exception);
+			throw(new $exceptionType($exception->getMessage(), 0, $exception));
+		}
+		//convert and store the cookbookRecipeId
+		$this->cookbookRecipeId = $uuid;
+	}
+
+	/**
+	 * accessor method for cookbookUserId
+	 * @return Uuid value of cookbookUserId
+	 */
+	public function getCookbookUserId(): Uuid {
+		return ($this->cookbookUserId);
+	}
+
+	/**
+	 * mutator method for cookbookUserId
+	 *
+	 * @param Uuid | string $newCookbookUserId value of cookbookUserId
+	 * @throws \RangeException if $newCookbookUserId is not positive
+	 * @throws \TypeError if the cookbookUserId is not a string
+	 */
+	public function setCookbookUserId($cookbookUserId): void {
+		try {
+			$uuid = self::validateUuid($newCookbookUserId);
+		} catch(\InvalidArgumentException | \RangeException | \Exception | \TypeError $exception) {
+			$exceptionType = get_class($exception);
+			throw(new $exceptionType($exception->getMessage(), 0, $exception));
+		}
+		//convert and store the cookbookUserId
+		$this->cookbookUserId = $uuid;
+	}
+	/**
+	 * inserts cookbook into mySQL
+	 * @param \PDO $pdo
+	 * @throws \PDOException when mySQL related errors occur
+	 * @throws \TypeError if $pdo is not a PDO connection object
+	 */
+	public function insert(\PDO $pdo) : void {
+		$query = "insert into cookbook(cookbookRecipeId, cookbookUserId) values (:cookbookRecipeId, :cookbookUserId)";
+  		$statement = $pdo->prepare($query);
+  		$paramaters = ["cookbookRecipeId" => $this->cookbookRecipeId->getBytes(), $this->cookbookUserId];
+  		$statement->execute($paramaters);
 	}
 }
