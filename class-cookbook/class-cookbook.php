71c9cfc762475f7e47b338ec9c3c8cf7f55b7d57
Samuel Nelson <snelson54@cnm.edu>
2019-05-08 11:00:57 -0600

    Added attributes and start of contruct function

---
+++ class-cookbook/class-cookbook.php
@@ -0,0 +1,26 @@
+<?php
+class Cookbook {
+	use ValidateUuid;
+	/**
+	 * id for this cookbook recipe;
+	 * @var Uuid $cookbookRecipeId
+	 */
+	private $cookbookRecipeId;
+	/**
+	 *
+	 * @var string for cookbookRecipeId
+	 */
+	private $cookbookUserId;
+	/**
+	 * @var string for cookbookUserId
+	 * @var cookbookUserId
+	 */
+	/**
+	 * constructor for author
+	 */
+	public function __construct(string $newCookbookRecipeId, string $newCookbookUserId) {
+		try {
+
+		}
+	}
+}
