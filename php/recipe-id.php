2159dfff9de6430c95a1f2df67cee534b8f8bd6d
Eric Martinez <emartinez451@cnm.edu>
2019-05-08 11:22:41 -0600

    creation of get recip by recipe id file

--- php/recipe-id.php
+++ php/recipe-id.php
@@ -41,3 +41,13 @@ $statement->execute($parameters);
 	}
 	return($recipe);
 }
+
+/**
+ * gets all recipes
+ *
+ * @param \PDO $pdo PDO connection object
+ * @return \SplFixedArray SplFixedArray of recipes found or null if not found
+ *
+ * @throws \PDOException when mySQL related errors occur
+ * @throws \TypeError when variables are not the correct data type
+ **/
