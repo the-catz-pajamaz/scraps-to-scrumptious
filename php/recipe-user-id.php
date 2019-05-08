f83dcf29210c1b7ac8009d323c4c4439fbdfbb57
Eric Martinez <emartinez451@cnm.edu>
2019-05-08 11:23:50 -0600

    updating recip id and recipe user id

--- php/recipe-user-id.php
+++ php/recipe-user-id.php
@@ -1,1 +1,11 @@
 <?php
+
+/**
+ * gets all recipes
+ *
+ * @param \PDO $pdo PDO connection object
+ * @return \SplFixedArray SplFixedArray of recipes found or null if not found
+ *
+ * @throws \PDOException when mySQL related errors occur
+ * @throws \TypeError when variables are not the correct data type
+ **/
