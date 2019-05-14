e88e74a6f8163d3541208e0389c213ffd8fb2c7c
Eric Martinez <emartinez451@cnm.edu>
2019-05-14 15:19:37 -0600

    Rearranged docblocks

--- php/Classes/ValidateUuid.php
+++ php/Classes/ValidateUuid.php
@@ -1,6 +1,6 @@
 <?php
 namespace theCatzPajamaz\ScrapsToScrumptious;
-require_once(dirname(__DIR__, 2) . "/autoload.php");
+require_once(dirname(__DIR__) . "/vendor/autoload.php");
 use Ramsey\Uuid\Uuid;
 /**
  * Trait to validate a uuid
