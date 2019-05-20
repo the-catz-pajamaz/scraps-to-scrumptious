07e031c2f09ca195b1eea63656d6c1ee70c979fa
Eric Martinez <emartinez451@cnm.edu>
2019-05-19 20:46:06 -0600

    testing grabbing a recipe by recipe description

--- php/Classes/Test/RecipeTest.php
+++ php/Classes/Test/RecipeTest.php
@@ -160,4 +160,32 @@ class RecipeTest extends scrapsToScrumptiousTest {
 
 	}
 
+	/**
+	 * test grabbing a Recipe by recipe content
+	 **/
+	public function testGetValidRecipeByRecipeDescription() : void {
+		// count the number of rows and save it for later
+		$numRows = $this->getConnection()->getRowCount("recipe");
+
+		// create a new Recipe and insert to into mySQL
+		$recipeId = generateUuidV4();
+		$recipe = new Recipe($recipeId, $this->recipe->getRecipeId(), $this->VALID_RECIPEDESCRIPTION);
+		$recipe->insert($this->getPDO());
+
+		// grab the data from mySQL and enforce the fields match our expectations
+		$results = Recipe::getRecipeByRecipeDescription($this->getPDO(), $recipe->getRecipeDescription());
+		$this->assertEquals($numRows + 1, $this->getConnection()->getRowCount("recipe"));
+		$this->assertCount(1, $results);
+
+		// enforce no other objects are bleeding into the test
+		$this->assertContainsOnlyInstancesOf("Edu\\Cnm\\ScrapsToScrumptios\\Recipe", $results);
+
+		// grab the result from the array and validate it
+		$pdoRecipe = $results[0];
+		$this->assertEquals($pdoRecipe->getRecipeId(), $recipeId);
+		$this->assertEquals($pdorecipe->getRecipeUserId(), $this->recipe->getRecipeId());
+		$this->assertEquals($pdoRecipe->getRecipeDescription(), $this->VALID_RECIPEDESCRIPTION);
+
+	}
+
 
