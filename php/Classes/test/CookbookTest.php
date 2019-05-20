b00b65777c57b47b36ca1bebcd1ba41a52c3ca12
Samuel Nelson <snelson54@cnm.edu>
2019-05-19 22:56:14 -0600

    Added docblock and started class

---
+++ php/Classes/test/CookbookTest.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace theCatzPajamaz\scrapsToScrumptious;
+require_once ("autoload.php");
+
+require_once(dirname(__DIR__) . "/validateuuid.php");
+
+use theCatzPajamaz\scrapsToScrumptious\{User, Recipe, Cookbook};
+
+/**
+ * Full PHP Unit test for the Cookbook class
+ *
+ * This is a full PHPUnit test of the Cookbook class. It is full because all mySQL and PDO enabled methods
+ * are tested for both invalid and valid inputs.
+ *
+ * @see \theCatzPajamaz\scrapsToScrumptious\Cookbook
+ * @author Samuel Nelson <snelson54@cnm.edu>
+ */
+
+class CookbookTest extends ScrapsToScrumptiousTest {
+
+	/**
+	 * User that owns the Cookbook; this is a foreign key relations
+	 * @var User $user
+	 */
+	protected $user;
+
+	/**
+	 *
+	 */
+}
