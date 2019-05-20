99e2ee9d6b86044efd3da3615c116a9123dae6dc
Samuel Nelson <snelson54@cnm.edu>
2019-05-19 22:49:27 -0600

    Added namespace

---
+++ php/Classes/test/class-cookbook-test.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace theCatzPajamaz\scrapsToScrumptious;
+require_once ("autoload.php");
+
+require_once(dirname(__DIR__) . "/validateuuid.php");
+
+use theCatzPajamaz\scrapsToScrumptious\{User, Recipe, Cookbook};
+
+
