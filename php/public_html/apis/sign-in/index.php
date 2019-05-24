34bda3d8a2c3b21c69e6af9c9150e7bb229d3729
Samuel Nelson <snelson54@cnm.edu>
2019-05-24 09:50:26 -0600

    Added require onces

--- php/public_html/apis/sign-in/index.php
+++ php/public_html/apis/sign-in/index.php
@@ -1,1 +1,13 @@
 <?php
+require_once dirname(__DIR__, 3) . "/classes/autoload.php";
+require_once("/etc/apache2/capstone-mysql/Secrets.php");
+require_once dirname(__DIR__, 3) . "/lib/xsrf.php";
+require_once dirname(__DIR__, 3) . "/lib/jwt.php";
+require_once("/etc/apache2/capstone-mysql/Secrets.php");
+
+use TheCatzPajamaz\ScrapsToScrumptious\{User, Recipe, Cookbook};
+
+/**
+ * api for handing sign-in
+ */
+
