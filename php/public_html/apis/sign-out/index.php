7195de82c0f8be9fd8a29f9bb7b91cb870a460c6
Eric Martinez <emartinez451@cnm.edu>
2019-05-24 11:27:17 -0600

    added index to sign-out

---
+++ php/public_html/apis/sign-out/index.php
@@ -0,0 +1,41 @@
+<?php
+require_once dirname(__DIR__, 3) . "/classes/autoload.php";
+require_once dirname(__DIR__, 3) . "/lib/xsrf.php";
+/**
+ * api for signing out
+ *
+ * @author Eric Martinez
+ * @version 1.0
+ **/
+//verify the xsrf challenge
+if(session_status() !== PHP_SESSION_ACTIVE){
+	session_start();
+}
+//prepare default error message
+$reply = new stdClass();
+$reply->status = 200;
+$reply->data = null;
+try {
+	//grab the mySQL connection
+	//determine which HTTP method was used
+	$method = array_key_exists("HTTP_X_HTTP_METHOD", $_SERVER) ? $_SERVER["HTTP_X_HTTP_METHOD"] : $_SERVER["REQUEST_METHOD"];
+	if($method === "GET") {
+		$_SESSION = [];
+		$reply->message = "You are now signed out.";
+	}
+	else {
+		throw (new \InvalidArgumentException("Invalid HTTP method request"));
+	}
+} catch(Exception $exception) {
+	$reply->status = $exception->getCode();
+	$reply->message = $exception->getMessage();
+} catch(TypeError $typeError) {
+	$reply->status = $exception->getCode();
+	$reply->message = $exception->getMessage();
+}
+header("Content-type: application/json");
+if($reply->data === null) {
+	unset($reply->data);
+}
+//encode and return reply to front end caller
+echo json_encode($reply);
