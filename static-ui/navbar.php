8aec8d5195f6ea35331092972c46d7c780592183
Samuel Nelson <snelson54@cnm.edu>
2019-05-24 11:57:26 -0600

    Attempted to get search bar to the right of navbar, not working

--- static-ui/navbar.php
+++ static-ui/navbar.php
@@ -43,15 +43,7 @@
 					<a class="nav-item nav-link" href="#">Cookbook</a>
 					<a class="nav-item nav-link" href="#">Login</a>
 					<a class="nav-item nav-link" href="#">Upload</a>
-					<div class="col-sm-3 col-md-3 mr-auto">
-						<form class="navbar-form" role="search">
-							<div class="input-group">
-								<input type="text" class="form-control" placeholder="Search" name="q">
-								<div class="input-group-btn">
-									<button class="btn btn-default" type="submit"><i class="glyphicon glyphicon-search"></i></button>
-								</div>
-							</div>
-						</form>
+					<input type="text" placeholder="Search..">
 					</div>
 				</div>
 			</div>
