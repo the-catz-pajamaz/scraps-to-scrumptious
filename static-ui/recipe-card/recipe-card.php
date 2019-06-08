08e87fb0885b20555cf39c48dc42755db54e1946
Samuel Nelson <snelson54@cnm.edu>
2019-06-08 12:27:02 -0600

    Added bootstrap

--- static-ui/recipe-card/recipe-card.php
+++ static-ui/recipe-card/recipe-card.php
@@ -1,4 +1,3 @@
-
 <!DOCTYPE html>
 <html lang="en">
 	<head>
@@ -17,9 +16,14 @@
 		<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js"></script>
 		<script src="https://cdn.jsdelivr.net/parallax.js/1.4.2/parallax.min.js"></script>
 
-		<script src="https://code.jquery.com/jquery-3.4.1.min.js" integrity="sha256-CSXorXvZcTkaix6Yvo6HppcZGetbYMGWSFlBw8HfCJo=" crossorigin="anonymous"></script>
+		<script src="https://code.jquery.com/jquery-3.4.1.min.js"
+				  integrity="sha256-CSXorXvZcTkaix6Yvo6HppcZGetbYMGWSFlBw8HfCJo=" crossorigin="anonymous"></script>
 
-		<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous"><script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+		<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"
+				integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+		<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"
+				  integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM"
+				  crossorigin="anonymous"></script>
 
 		<!-- Google Fonts -->
 		<link href="https://fonts.googleapis.com/css?family=Montserrat&display=swap" rel="stylesheet">
@@ -30,14 +34,71 @@
 		<title>Home</title>
 	</head>
 	<body>
-		<div class="container">
-		<div class="container" id="recipe-card">
-			<img src="egg.jpg" alt="Avatar" class="image">
-			<div class="overlay">
-				<p></p>
-				<div class="text" id="text"><a href="egg.jpg">Hello World</a></div>
+
+		<section id="recipe-cards">
+			<div class="container mt-5">
+				<div class="row">
+					<div class="container justify-content-center">
+						<div class="row">
+							<div class="col-lg-3 col-sm-10">
+								<!--					<img class="img-fluid mx-auto d-block rounded-circle my-3" src="stripes.png" width="200px"-->
+								<!--						  height="200px">-->
+								<div class="card">
+									<div class="container">
+										<div class="container" id="recipe-card">
+											<img src="egg.jpg" alt="Avatar" class="image">
+											<div class="overlay">
+												<div class="text" id="text"><a href="egg.jpg">Hello World</a>
+												</div>
+											</div>
+										</div>
+									</div>
+								</div>
+							</div>
+						</div>
+					</div>
+					<div class="container justify-content-center">
+						<div class="row">
+							<div class="col-lg-3 col-sm-10">
+								<!--					<img class="img-fluid mx-auto d-block rounded-circle my-3" src="stripes.png" width="200px"-->
+								<!--						  height="200px">-->
+								<div class="card">
+									<div class="container">
+										<div class="container" id="recipe-card">
+											<img src="egg.jpg" alt="Avatar" class="image">
+											<div class="overlay">
+												<div class="text" id="text"><a href="egg.jpg">Hello World</a>
+												</div>
+											</div>
+										</div>
+									</div>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
 			</div>
-		</div>
-		</div>
+		</section>
 	</body>
 </html>
+
+
+<!--<section>-->
+<!--	<div class="container justify-content-center">-->
+<!--		<div class="row">-->
+<!--			<div class="col-lg-3 col-sm-10">-->
+<!--				<img class="img-fluid mx-auto d-block rounded-circle my-3" src="stripes.png" width="200px" height="200px">-->
+<!--				<div class="card">-->
+<!--					<div class="container">-->
+<!--						<div class="container" id="recipe-card">-->
+<!--							<img src="egg.jpg" alt="Avatar" class="image">-->
+<!--							<div class="overlay">-->
+<!--								<div class="text" id="text"><a href="egg.jpg">Hello World</a></div>-->
+<!--							</div>-->
+<!--						</div>-->
+<!--					</div>-->
+<!--				</div>-->
+<!--			</div>-->
+<!---->
+<!--		</div>-->
+<!--</section>-->
